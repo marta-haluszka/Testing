b5b8f092bd82c17359e7b2f26132a349cc3ab839
Marta Haluszka <marta.haluszka@gmail.com>
2022-08-15 19:11:26 +0200

    Add README

---
+++ README.py
@@ -0,0 +1,14 @@
+# Me and my IT exercises
+## About me
+Hey:)
+My name is Marta Haluszka.This repository shows my first steps in the IT world.
+I know quite a few tools and I have some tech skills which are nessesary to be good tester.
+In my work I had alerdy test web application and report the bugs I found in Jira
+I am precise and focused on details, determined and try to improve my knowledge every day.
+In this repository I put some example of my work.
+Also, I am learning Python and I am putting my first program into this repository.
+### Program to creating a character in the RPG Warhammer
+I will try to add as many possibilities to this program as I can, but these are my first steps in programming.
+In the first phase of creating this code, user will need polyhedral dice and a character card to create a character.
+In the next phase, I plan to add a library to randomly generate dice rolls.
+The next part will be combining the two previous phases so that the user can choose the best method for him to create a character :)
